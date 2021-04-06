b47addbe14427647d118e8a1459d50b4ddd7dd7d
Kat Huang <khuang129@gmail.com>
2021-04-06 14:58:29 -0700

    initial commit

---
+++ carousel/src/Carousel.test.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import Carousel from "./Carousel";
+
+it("works when you click on the right arrow", function() {
+  const { container } = render(<Carousel />);
+
+  // expect the first image to show, but not the second
+  expect(
+    container.querySelector('img[alt="Photo by Richard Pasquarella on Unsplash"]')
+  ).toBeInTheDocument();
+  expect(
+    container.querySelector('img[alt="Photo by Pratik Patel on Unsplash"]')
+  ).not.toBeInTheDocument();
+
+  // move forward in the carousel
+  const rightArrow = container.querySelector(".fa-chevron-circle-right");
+  fireEvent.click(rightArrow);
+
+  // expect the second image to show, but not the first
+  expect(
+    container.querySelector('img[alt="Photo by Richard Pasquarella on Unsplash"]')
+  ).not.toBeInTheDocument();
+  expect(
+    container.querySelector('img[alt="Photo by Pratik Patel on Unsplash"]')
+  ).toBeInTheDocument();
+});
