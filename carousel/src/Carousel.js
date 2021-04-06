94cdd32a6794adda2518ebe21196aab4008610d6
David Zemel <zemel96@gmail.com>
2021-04-06 19:26:44 -0400

    part 3 complete

--- carousel/src/Carousel.js
+++ carousel/src/Carousel.js
@@ -10,25 +10,20 @@ function Carousel(props) {
   const card = props.cardData[cardIdx];
   const total = props.cardData.length;
   const goForward = () => setCardIdx(cardIdx + 1);
+  const goBackward = () => setCardIdx(cardIdx - 1);
 
   return (
     <div className="Carousel">
       <h1>{props.title}</h1>
       <div className="Carousel-main">
-        <i
-          className="fas fa-chevron-circle-left fa-2x"
-          onClick={goForward}
-        />
+        <i className="fas fa-chevron-circle-left fa-2x" onClick={goBackward} />
         <Card
           caption={card.caption}
           src={card.src}
           currNum={cardIdx + 1}
           totalNum={total}
         />
-        <i
-          className="fas fa-chevron-circle-right fa-2x"
-          onClick={goForward}
-        />
+        <i className="fas fa-chevron-circle-right fa-2x" onClick={goForward} />
       </div>
     </div>
   );
@@ -38,18 +33,18 @@ Carousel.defaultProps = {
   cardData: [
     {
       src: image1,
-      caption: "Photo by Richard Pasquarella on Unsplash"
+      caption: "Photo by Richard Pasquarella on Unsplash",
     },
     {
       src: image2,
-      caption: "Photo by Pratik Patel on Unsplash"
+      caption: "Photo by Pratik Patel on Unsplash",
     },
     {
       src: image3,
-      caption: "Photo by Josh Post on Unsplash"
-    }
+      caption: "Photo by Josh Post on Unsplash",
+    },
   ],
-  title: "Shells from far away beaches."
+  title: "Shells from far away beaches.",
 };
 
 export default Carousel;
--- carousel/src/Carousel.test.js
+++ carousel/src/Carousel.test.js
@@ -13,12 +13,14 @@ it("matches snapshot", function () {
 
 // end
 
-it("works when you click on the right arrow", function() {
+it("works when you click on the right arrow", function () {
   const { container } = render(<Carousel />);
 
   // expect the first image to show, but not the second
   expect(
-    container.querySelector('img[alt="Photo by Richard Pasquarella on Unsplash"]')
+    container.querySelector(
+      'img[alt="Photo by Richard Pasquarella on Unsplash"]'
+    )
   ).toBeInTheDocument();
   expect(
     container.querySelector('img[alt="Photo by Pratik Patel on Unsplash"]')
@@ -30,9 +32,40 @@ it("works when you click on the right arrow", function() {
 
   // expect the second image to show, but not the first
   expect(
-    container.querySelector('img[alt="Photo by Richard Pasquarella on Unsplash"]')
+    container.querySelector(
+      'img[alt="Photo by Richard Pasquarella on Unsplash"]'
+    )
   ).not.toBeInTheDocument();
   expect(
     container.querySelector('img[alt="Photo by Pratik Patel on Unsplash"]')
+  ).toBeInTheDocument();
+});
+
+it("works when you click on the left arrow", function () {
+  const { container } = render(<Carousel />);
+  const rightArrow = container.querySelector(".fa-chevron-circle-right");
+  fireEvent.click(rightArrow);
+  // expect the first image to show, but not the second
+  expect(
+    container.querySelector('img[alt="Photo by Pratik Patel on Unsplash"]')
+  ).toBeInTheDocument();
+  expect(
+    container.querySelector(
+      'img[alt="Photo by Richard Pasquarella on Unsplash"]'
+    )
+  ).not.toBeInTheDocument();
+
+  // move forward in the carousel
+  const leftArrow = container.querySelector(".fa-chevron-circle-left");
+  fireEvent.click(leftArrow);
+
+  // expect the second image to show, but not the first
+  expect(
+    container.querySelector('img[alt="Photo by Pratik Patel on Unsplash"]')
+  ).not.toBeInTheDocument();
+  expect(
+    container.querySelector(
+      'img[alt="Photo by Richard Pasquarella on Unsplash"]'
+    )
   ).toBeInTheDocument();
 });
